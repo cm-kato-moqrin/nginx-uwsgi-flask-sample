5040d4479002922f3e9a0667b280cd7788476700
cmoqrin <kato.teiji@classmethod.jp>
2021-09-11 19:55:28 +0900

    meshnize

--- app_flask/app.py
+++ app_flask/app.py
@@ -1,18 +1,25 @@
 from flask import Flask
+from flask_restful import Resource, Api
 import logging
+import os
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
 app = Flask(__name__)
 app.logger.setLevel(logging.INFO)
+api = Api(app)
 xray_recorder.configure(service='demo flask-app')
 XRayMiddleware(app, xray_recorder)
 
 
-@app.route("/")
-def hello():
-    app.logger.info('Hello called!')
-    return "<h1 style='color:blue'>Hello There!</h1>"
+class Hello(Resource):
+    def get(self):
+        name = os.environ["NAME"]
+        app.logger.info('Hello called!')
+        return {'hello': name}
 
+
+api.add_resource(Hello, '/')
 
 if __name__ == "__main__":
     app.run()
